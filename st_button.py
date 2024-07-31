4fbbd9e007821fd4e81f834f41c17805e18fa372
Hiram-hub <hiram.hdzr@gmail.com>
2024-07-30 23:21:36 -0600

    Add files via upload

---
+++ st_button.py
@@ -0,0 +1,12 @@
+####################################################################################################
+# Se explica la función "st.button"
+####################################################################################################
+
+import streamlit as st
+
+st.header('st.button')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
